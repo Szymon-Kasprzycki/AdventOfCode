8e2178886212a61f564fc243d64b475d54123979
Szymon Kasprzycki <66119938+Szymon-Kasprzycki@users.noreply.github.com>
2022-05-22 01:22:12 +0200

    Year 2015 files and solution for first 5 days
    First 5 days of year 2015 solved, all files of this year added

---
+++ 2015/23/code.py
@@ -0,0 +1,14 @@
+# Advent of code Year 2015 Day 23 solution
+# Author = Szymon Kasprzycki
+# Date = May 2022
+
+with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
+    input_data = input_file.read()
+
+
+
+print(f"Part One: {str(None)}")
+
+
+
+print(f"Part Two: {str(None)}")
