15f4543a674f57a55a967e10ff22126577ae6e42
Szymon Kasprzycki <66119938+Szymon-Kasprzycki@users.noreply.github.com>
2022-07-04 21:29:19 +0200

    [ADD] Solutions for days 16 and 17

--- 2015/16/code.py
+++ 2015/16/code.py
@@ -1,14 +1,64 @@
 # Advent of code Year 2015 Day 16 solution
 # Author = Szymon Kasprzycki
-# Date = May 2022
+# Date = June 2022
 
-with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
-    input_data = input_file.read()
+with open((__file__.rstrip("code.py") + "input.txt"), 'r') as input_file:
+    input_data = input_file.readlines()
+
+check_data = {
+    'children': 3,
+    'cats': 7,
+    'samoyeds': 2,
+    'pomeranians': 3,
+    'akitas': 0,
+    'vizslas': 0,
+    'goldfish': 5,
+    'trees': 3,
+    'cars': 2,
+    'perfumes': 1
+}
 
 
+def read_aunts_data():
+    aunts_data = {}
+    for line in input_data:
+        _, n, a, an, b, bn, c, cn = line.strip().replace(':', '').replace(',', '').split()
+        aunts_data[n] = {
+            a: int(an),
+            b: int(bn),
+            c: int(cn)
+        }
+    return aunts_data
 
-print(f"Part One: {str(None)}")
+
+def check(aunt_data: dict, check_data: dict, with_bugs: bool = False):
+    passed = [False, False, False]
+    x = 0
+    for key, value in aunt_data.items():
+        if (key not in ['cats', 'trees', 'pomeranians', 'goldfish'] and value == check_data[key] and with_bugs) or (
+                value == check_data[key] and not with_bugs):
+            passed[x] = True
+        elif key in ['cats', 'trees'] and value > check_data[key]:
+            passed[x] = True
+        elif key in ['pomeranians', 'goldfish'] and value < check_data[key]:
+            passed[x] = True
+        x += 1
+    return all(passed)
 
 
+def part_one():
+    aunts_data = read_aunts_data()
+    for aunt_number, aunt_data in aunts_data.items():
+        if check(aunt_data, check_data, False):
+            return aunt_number
 
-print(f"Part Two: {str(None)}")
+
+def part_two():
+    aunts_data = read_aunts_data()
+    for aunt_number, aunt_data in aunts_data.items():
+        if check(aunt_data, check_data, True):
+            return aunt_number
+
+
+print(f"Part One: {str(part_one())}")
+print(f"Part Two: {str(part_two())}")
--- 2015/17/code.py
+++ 2015/17/code.py
@@ -1,14 +1,34 @@
 # Advent of code Year 2015 Day 17 solution
 # Author = Szymon Kasprzycki
-# Date = May 2022
+# Date = June 2022
+from itertools import combinations
 
-with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
-    input_data = input_file.read()
-
-
-
-print(f"Part One: {str(None)}")
+with open((__file__.rstrip("code.py") + "input.txt"), 'r') as input_file:
+    input_data = input_file.readlines()
 
 
+def parse_input_data():
+    containers = [int(x.strip()) for x in input_data]
+    return containers
 
-print(f"Part Two: {str(None)}")
+
+def get_containers_combinations(containers, target_capacity):
+    possibilities = [p for number_of_bottles in range(len(containers)) for p in
+                     combinations(containers, number_of_bottles) if sum(p) == target_capacity]
+    return possibilities
+
+
+def part_one():
+    containers = parse_input_data()
+    possibilities = get_containers_combinations(containers, 150)
+    return len(possibilities)
+
+
+def part_two():
+    containers = parse_input_data()
+    possibilities = get_containers_combinations(containers, 150)
+    return len([p for p in possibilities if len(p) == len(min(possibilities, key=lambda x: len(x)))])
+
+
+print(f"Part One: {str(part_one())}")
+print(f"Part Two: {str(part_two())}")
